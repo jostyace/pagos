0d779049f46dfd946353f959f76a169f6185e680
jostyace <jostyace@gmail.com>
2024-05-06 22:42:19 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'], // Añade otros activos estáticos según sea necesario
+      manifest: {
+        name: 'Nombre de tu Aplicación',
+        short_name: 'AppCorta',
+        description: 'Descripción de tu Aplicación',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        start_url: '.',
+        display: 'standalone',
+        orientation: 'portrait'
+      }
+    })
+  ]
 })
